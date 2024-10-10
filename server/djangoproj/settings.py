f4d391878398414b968911568a082e760fae29ba
Joey <levinjoey@gmail.com>
2024-10-10 13:25:34 -0400

    View dealers and add reviews

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 # Add app root URL
-ALLOWED_HOSTS=['localhost','https://levinjoey-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://levinjoey-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS=['localhost','https://levinjoey-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS=['https://levinjoey-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 
 REST_FRAMEWORK = {
